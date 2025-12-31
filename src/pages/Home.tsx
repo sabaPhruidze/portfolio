20d6755a68002c1450195e5685a0aa16bc9c22e6
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-31 20:09:59 +0400

    added images and I will continue tomorrow rest

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import Introduction from "../components/home/Introduction";
+import Skills from "../components/home/Skills";
 const Home = () => {
   return (
     <div>
       <Introduction />
+      <Skills />
     </div>
   );
 };
