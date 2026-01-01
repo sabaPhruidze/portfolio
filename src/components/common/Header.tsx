2bd265df3e0cbe59439c3a096fd8092308a7d14e
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-01 13:24:44 +0400

    changed w-screen to w-full in order to solve the scrollbar issue and corrected the KA paragraph design

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import moon from "../../assets/moon.png";
 import menu from "../../assets/menu.png";
 const Header = () => {
   return (
-    <div className="w-screen h-20 flex flex-row bg-white items-center px-5 justify-between">
+    <div className="w-full h-20 flex flex-row bg-white items-center px-5 justify-between">
       <section id="left-part" className="flex flex-row">
         <div className="w-10 h-10 bg-blue-700 rounded-md flex justify-center items-center shadow-md shadow-blue-200">
           <p className="text-white font-extrabold scale-y-[1.5] translate-y-[-2px]">
@@ -21,7 +21,7 @@ const Header = () => {
           <div className="w-0.25 h-5 bg-gray-400" />
           <p className="font-bold">EN</p>
           <div className="w-0.25 h-5 bg-gray-400" />
-          <p>KA</p>
+          <p className="text-gray-500 font-bold">KA</p>
         </div>
         <div className="h-10 w-10 bg-gray-300 rounded-md flex justify-center items-center">
           <img src={menu} alt="symbol of menu" className="w-5 h-5 " />
