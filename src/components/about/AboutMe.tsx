a4a45959b514a94159aaaa95ef99b1c26bf98b7a
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-04 16:41:48 +0400

    now I have to add 2 div with location and avaliability

---
+++ src/components/about/AboutMe.tsx
@@ -0,0 +1,28 @@
+import user from "../../assets/user.png";
+
+const AboutMe = () => {
+  return (
+    <section className=" px-10 w-full h-300 bg-gray-300">
+      <div className="flex flex-row pt-10 items-center">
+        <img src={user} alt="user icon" className="w-5 h-5 mr-3" />
+        <h2 className="font-semibold text-xl">About Me</h2>
+      </div>
+      <p className="text-sm text-gray-500 mt-5">
+        My journey into technology began with a curiosity for how things work
+        under the hood.What started as simple HTML edits on gaming forums has
+        evolved into a full-blown career engineering complex web applications.
+        <br /> I believe that great software is the intersection of clean code,
+        intuitive design, and solving real user problems. I don’t just write
+        code; I think about the product holistically.Whether it’s optimizing
+        database queries or tweaking micro-interactions, I’m committed to
+        quality at every layer of the stack.
+        <br /> When I’m not coding, you can find me contributing to open-source
+        projects, mentoring aspiring developers, or exploring the mountains. I’m
+        a lifelong learner who thrives in environments that challenge me to
+        grow.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import Experience from "../components/about/Experience";
+import AboutMe from "../components/about/AboutMe";
 const About = () => {
   return (
     <div>
       <Experience />
+      <AboutMe />
     </div>
   );
 };
