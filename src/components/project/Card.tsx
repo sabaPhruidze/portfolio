f64336a765ffacc1b5ce4a355209ebe26dd2fe13
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-06 14:50:02 +0400

    third page for mobile visually finished

--- src/components/project/Card.tsx
+++ src/components/project/Card.tsx
@@ -1,49 +1,56 @@
-import analytics from "../../assets/card/analytics.jpg";
-import analyze from "../../assets/card/analyze.png";
 import eye from "../../assets/card/eye.png";
-const Card = () => {
+import type { Data } from "../../pages/Project";
+type CardProps = {
+  DATA: Data[];
+};
+const Card = ({ DATA }: CardProps) => {
   return (
-    <div className="w-full bg-white mt-10 rounded-xl pb-10">
-      <img
-        src={analytics}
-        alt="analytics"
-        className="w-full h-40 object-cover rounded-xl"
-      />
-      <div className="px-5">
-        <div className="flex flex-row mt-5 justify-between">
-          <h3 className="font-bold text-xl ">E-commerce Analytics</h3>
-          <div className="w-8 h-8 bg-gray-100 flex justify-center items-center rounded-xl">
-            <img src={analyze} alt="analyze icon" className="w-5 h-5 " />
+    <>
+      {DATA?.map((item: Data) => (
+        <div className="w-full bg-white mt-10 rounded-xl pb-10" key={item.id}>
+          <img
+            src={item.mainImage}
+            alt={item.mainImageAlt}
+            className="w-full h-40 object-cover rounded-xl"
+          />
+          <div className="px-5">
+            <div className="flex flex-row mt-5 justify-between">
+              <h3 className="font-bold text-xl ">{item.title}</h3>
+              <div className="w-8 h-8 bg-gray-100 flex justify-center items-center rounded-xl">
+                <img
+                  src={item.iconImage}
+                  alt={item.iconImageAlt}
+                  className="w-5 h-5 "
+                />
+              </div>
+            </div>
+            <p className="mt-5 text-gray-500">{item.content}</p>
+            <div>
+              <div className="flex flex-row gap-3 mt-3">
+                {item.tag.map((ta) => (
+                  <p className="text-gray-700 bg-gray-200 px-2 py-0.5 rounded-md">
+                    {ta}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 mt-10">
+              <button className="w-[90%] py-1 bg-blue-700 text-white rounded-md cursor-pointer flex flex-row justify-center gap-2 items-center">
+                <img
+                  src={eye}
+                  alt="eye icon"
+                  className="w-8 h-8 translate-y-0.5"
+                />
+                <p>Live Demo</p>
+              </button>
+              <button className="w-[10%] py-1 border border-gray-500 text-gray-700 rounded-md cursor-pointer">
+                &lt;&gt;
+              </button>
+            </div>
           </div>
         </div>
-        <p className="mt-5 text-gray-500">
-          A comprehensive dashboard for tracking sales, inventory, and customer
-          metrics in real-time. Includes dark mode and export features.
-        </p>
-        <div>
-          <div className="flex flex-row gap-3 mt-3">
-            <p className="text-gray-700 bg-gray-200 px-2 py-0.5 rounded-md">
-              React
-            </p>{" "}
-            <p className="text-gray-700 bg-gray-200 px-2 py-0.5 rounded-md">
-              Typescript
-            </p>
-            <p className="text-gray-700 bg-gray-200 px-2 py-0.5 rounded-md">
-              Tailwind
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row gap-2 mt-10">
-          <button className="w-[90%] py-1 bg-blue-700 text-white rounded-md cursor-pointer flex flex-row justify-center gap-2 items-center">
-            <img src={eye} alt="eye icon" className="w-8 h-8 translate-y-0.5" />
-            <p>Live Demo</p>
-          </button>
-          <button className="w-[10%] py-1 border border-gray-500 text-gray-700 rounded-md cursor-pointer">
-            &lt;&gt;
-          </button>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,9 +1,89 @@
 import Card from "../components/project/Card";
 
+import analytics from "../assets/card/e-commerce.png";
+import analytics_icon from "../assets/card/analytics_icon.png";
+
+import finance_tracker from "../assets/card/tracker.png";
+import finance_tracker_icon from "../assets/card/wallet_icon.png";
+
+import api from "../assets/card/devdocs_api.png";
+import api_icon from "../assets/card/api_icon.png";
+
+import threed_icon from "../assets/card/3d (1).png";
+import threed from "../assets/card/3d.png";
+
+export interface Data {
+  id: number;
+  mainImage: string;
+  mainImageAlt: string;
+  title: string;
+  iconImage: string;
+  iconImageAlt: string;
+  content: string;
+  tag: string[];
+  demo: string;
+  code: string;
+}
+
 const Project = () => {
   const show = ["All", "Web App", "Mobile", "Open Source", "Design System"];
+
+  const DATA: Data[] = [
+    {
+      id: 1,
+      mainImage: analytics,
+      mainImageAlt: "analyze image",
+      title: "E-commerce Analytics",
+      iconImage: analytics_icon,
+      iconImageAlt: "analyze icon",
+      content:
+        " A comprehensive dashboard for tracking sales, inventory, and customer metrics in real-time. Includes dark mode and export features.",
+      tag: ["React", "Typescript", "Tailwind"],
+      demo: "https://www.mseprinting.com/",
+      code: "https://github.com/sabaPhruidze/mseprint",
+    },
+    {
+      id: 2,
+      mainImage: finance_tracker,
+      mainImageAlt: "finance tracker image",
+      title: "Finance Tracker",
+      iconImage: finance_tracker_icon,
+      iconImageAlt: "finance tracker icon",
+      content:
+        " Personal finance management app allowing users to track expenses, set budgets, and visualize spending habits over time.",
+      tag: ["Next.js", "Prisma", "PosgreSQL"],
+      demo: "https://www.mseprinting.com/",
+      code: "https://github.com/sabaPhruidze/mseprint",
+    },
+    {
+      id: 3,
+      mainImage: api,
+      mainImageAlt: "DevDocs API image",
+      title: "DevDocs API",
+      iconImage: api_icon,
+      iconImageAlt: "DevDocs API icon",
+      content:
+        " A centralized documentation platform for internal developer tools, featuring search, versioning, and code snippets.",
+      tag: ["Vue", "Nuxt", "Algolia"],
+      demo: "https://www.mseprinting.com/",
+      code: "https://github.com/sabaPhruidze/mseprint",
+    },
+    {
+      id: 4,
+      mainImage: threed,
+      mainImageAlt: "3D image",
+      title: "3D Portfolio",
+      iconImage: threed_icon,
+      iconImageAlt: "3D icon",
+      content:
+        "An immersive 3D portfolio experience utilizing WebGL to create interactive environments and showcase creative work.",
+      tag: ["Three.js", "WebGL", "GSAP"],
+      demo: "https://www.mseprinting.com/",
+      code: "https://github.com/sabaPhruidze/mseprint",
+    },
+  ];
   return (
-    <section className="w-full h-250 bg-gray-300 px-10">
+    <section className="w-full h-650 bg-gray-300 px-10">
       <h2 className="text-4xl font-bold pt-10">Featured Projects</h2>
       <p className="font-semibold text-gray-500 mt-5">
         building digital experiences with clean code and modern design. Here is
@@ -19,7 +99,7 @@ const Project = () => {
           </button>
         ))}
       </div>
-      <Card />
+      <Card DATA={DATA} />
     </section>
   );
 };
