7785f305c7c33d9ade08e083ebb010ab3df246b0
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-08 15:42:42 +0400

    now it is the time to start card creating

--- src/components/skilllab/Main.tsx
+++ src/components/skilllab/Main.tsx
@@ -6,7 +6,32 @@ import advanced_theming_System from "../../assets/skillLab/advanced_theming_Syst
 import drag_drop from "../../assets/skillLab/drag_drop.png";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <section className="my-10">
+      <div className="flex flex-row gap-3">
+        <button className="py-2 px-5 bg-black text-white rounded-2xl">
+          All
+        </button>
+        <button className="py-2 px-5 bg-white border border-gray-500 rounded-2xl">
+          React Components
+        </button>
+        <button className="py-2 px-5 bg-white border border-gray-500 rounded-2xl">
+          Animations
+        </button>
+      </div>
+      <div className="flex flex-row gap-3 mt-5 mb-10">
+        <p>View:</p>
+        <div className="bg-white rounded-md">
+          <img
+            src={fourCube}
+            alt="four cube icon"
+            className="w-7 h-7 rounded-md"
+          />
+        </div>
+        <img src={sort} alt="sorting icon" className="w-6 h-6" />
+      </div>
+    </section>
+  );
 };
 
 export default Main;
