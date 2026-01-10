f72c1ffc8a636a30aa6fa857436026f5ff52589a
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-10 12:57:03 +0400

    subscribe part finished as well for mobile and now I move on on the last page

---
+++ src/components/skilllab/Subscribe.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Subscribe = () => {
+  return (
+    <section className="px-10 py-5 bg-blue-500 text-white rounded-xl">
+      <h2 className="font-bold text-2xl ">
+        Want to see how <br />
+        these are built?
+      </h2>
+      <p className="my-3 text-gray-200">
+        I write detailed breakdowns of my experiments. Subscribe to get notified{" "}
+        <br /> when I drop a new component.
+      </p>
+      <input
+        type="text"
+        placeholder="Enter your email"
+        className="py-3 px-5 bg-white/15 rounded-xl w-full placeholder:text-gray-300 outline-none transition-shadow duration-300
+    focus:ring-1 focus:ring-blue-300 focus:ring-offset-2 focus:ring-offset-transparent"
+      />
+      <div className="flex justify-center my-5 ">
+        <button className="py-3 px-5 bg-white text-blue-700 font-bold rounded-xl cursor-pointer hover:scale-105 active:scale-95 transition duration-300">
+          Subscribe
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Subscribe;
--- src/pages/SkillLab.tsx
+++ src/pages/SkillLab.tsx
@@ -1,10 +1,12 @@
 import Intro from "../components/skilllab/Intro";
 import Main from "../components/skilllab/Main";
+import Subscribe from "../components/skilllab/Subscribe";
 const SkillLab = () => {
   return (
     <div className="w-full px-10 py-20 bg-gray-300">
       <Intro />
       <Main />
+      <Subscribe />
     </div>
   );
 };
