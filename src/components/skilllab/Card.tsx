fc747a9cd665aa9be4c0cfd0e4ef769359e91f9c
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-10 12:25:02 +0400

    card finished and last part of this component will start now it will be subscribtion

---
+++ src/components/skilllab/Card.tsx
@@ -0,0 +1,73 @@
+import right from "../../assets/skillLab/right.png";
+import type { Data } from "./Main";
+
+type CardProps = {
+  DATA: Data[];
+};
+const Card = ({ DATA }: CardProps) => {
+  return (
+    <>
+      {DATA.map((item: Data) => (
+        <div
+          key={item.id}
+          className="w-full h-100 border border-gray-500 rounded-xl bg-white mt-10"
+        >
+          <img
+            src={item.src}
+            alt={item.alt}
+            className="w-full h-40 object-cover rounded-t-xl"
+          />
+          <div className="px-5">
+            <div className="flex flex-row gap-3 py-5">
+              {item.tags.map((tag) => (
+                <div
+                  className={`px-2 py-1 rounded-xl ${
+                    tag.id === 1
+                      ? "bg-blue-200"
+                      : tag.id === 3
+                      ? "bg-blue-100"
+                      : tag.id === 5
+                      ? "bg-orange-2s00"
+                      : "bg-gray-200"
+                  }`}
+                >
+                  <p
+                    className={`font-semibold text-sm ${
+                      tag.id === 1
+                        ? "text-blue-700"
+                        : tag.id === 3
+                        ? "text-blue-500"
+                        : tag.id === 5
+                        ? "text-orange-700"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    {tag.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+            <h2 className="font-bold text-xl mb-5">{item.title}</h2>
+            <p>{item.content}</p>
+            <div className="w-full h-0.5 bg-gray-300 my-3" />
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row">
+                <p className="mr-2 font-semibold">Try Demo</p>
+                <img
+                  src={right}
+                  alt="right icon"
+                  className="w-4 h-4 translate-y-1"
+                />
+              </div>
+              <p className="font-bold scale-y-[1.5] -translate-y-0.5 mr-2 text-gray-500">
+                &lt; &gt;
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Card;
--- src/components/skilllab/Main.tsx
+++ src/components/skilllab/Main.tsx
@@ -1,17 +1,29 @@
-import right from "../../assets/skillLab/right.png";
+import Card from "./Card";
 import sort from "../../assets/skillLab/sort.png";
 import infinite_scroll from "../../assets/skillLab/infinite_scroll.png";
 import fourCube from "../../assets/skillLab/4 cube.png";
 import advanced_theming_System from "../../assets/skillLab/advanced_theming_System.png";
 import drag_drop from "../../assets/skillLab/drag_drop.png";
 
+export interface Data {
+  id: number;
+  src: string;
+  alt: string;
+  tags: { id: number; name: string }[];
+  title: string;
+  content: string;
+}
+
 const Main = () => {
-  const DATA = [
+  const DATA: Data[] = [
     {
       id: 1,
       src: drag_drop,
       alt: "drap drop image",
-      tags: ["React DnD", "Sortable"],
+      tags: [
+        { id: 1, name: "React DnD" },
+        { id: 2, name: "Sortable" },
+      ],
       title: "Drag & Drop Kanban",
       content:
         "  A flud Drag-and-drop interface with optimistic updates and accessible keyboard...",
@@ -20,7 +32,10 @@ const Main = () => {
       id: 2,
       src: advanced_theming_System,
       alt: "advanced theming System image",
-      tags: ["Tailwind", "Context API"],
+      tags: [
+        { id: 3, name: "Tailwind" },
+        { id: 4, name: "Context API" },
+      ],
       title: "Advanced Theming System",
       content:
         "System-preference aware dark mode toggle with local storage persistence and CSS...",
@@ -29,7 +44,10 @@ const Main = () => {
       id: 3,
       src: infinite_scroll,
       alt: "infinite scroll image",
-      tags: ["React Query", "Intersection Observer"],
+      tags: [
+        { id: 5, name: "React Query" },
+        { id: 6, name: "Intersection Observer" },
+      ],
       title: "Infinite Scroll Hook",
       content:
         "A custom hook for infinite loading with intersection observer, featuring loading...",
@@ -48,7 +66,7 @@ const Main = () => {
           Animations
         </button>
       </div>
-      <div className="flex flex-row gap-3 mt-5 mb-10">
+      <div className="flex flex-row gap-3 mt-5 ">
         <p>View:</p>
         <div className="bg-white rounded-md">
           <img
@@ -59,42 +77,7 @@ const Main = () => {
         </div>
         <img src={sort} alt="sorting icon" className="w-6 h-6" />
       </div>
-      <div className="w-full h-100 border border-gray-500 rounded-xl bg-white">
-        <img
-          src={drag_drop}
-          alt="drap drop image"
-          className="w-full h-40 object-cover rounded-t-xl"
-        />
-        <div className="px-5">
-          <div className="flex flex-row gap-3 py-5">
-            <div className="px-2 py-1 bg-blue-300 rounded-xl">
-              <p className="text-blue-700 font-semibold text-sm">React DnD</p>
-            </div>
-            <div className="px-2 py-1 bg-gray-200 rounded-xl">
-              <p className="font-semibold text-gray-700 text-sm">Sortable</p>
-            </div>
-          </div>
-          <h2 className="font-bold text-xl mb-5">Drag & Drop Kanban</h2>
-          <p>
-            A flud Drag-and-drop interface with optimistic updates and
-            accessible keyboard...
-          </p>
-          <div className="w-full h-0.5 bg-gray-300 my-3" />
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row">
-              <p className="mr-2 font-semibold">Try Demo</p>
-              <img
-                src={right}
-                alt="right icon"
-                className="w-4 h-4 translate-y-1"
-              />
-            </div>
-            <p className="font-bold scale-y-[1.5] -translate-y-0.5 mr-2 text-gray-500">
-              &lt; &gt;
-            </p>
-          </div>
-        </div>
-      </div>
+      <Card DATA={DATA} />
     </section>
   );
 };
