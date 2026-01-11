5bcca20266c0c110bfe7288e776f0aa029276798
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-11 11:31:43 +0400

    now I will start creating form for contact page, but it will be react-hook-form zod and zodresolver in order to demonstrate what I have learne

---
+++ src/components/contact/InputBox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputBox = () => {
+  return (
+    <section className="bg-white">
+      <form>
+        <fieldset>sabsa</fieldset>
+      </form>
+    </section>
+  );
+};
+
+export default InputBox;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,11 @@
 import GetInTouch from "../components/contact/GetInTouch";
+import InputBox from "../components/contact/InputBox";
 
 const Contact = () => {
   return (
-    <div className="px-5 pt-10 bg-gray-50">
+    <div className="px-5 pt-10 bg-gray-50 h-200">
       <GetInTouch />
+      <InputBox />
     </div>
   );
 };
