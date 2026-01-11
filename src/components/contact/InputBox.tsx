674c74b7a665897e76af46c7a51620b094da6bf0
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-11 11:56:34 +0400

    finishd creating schema

--- src/components/contact/InputBox.tsx
+++ src/components/contact/InputBox.tsx
@@ -1,3 +1,25 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const getInTouchSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(3, { message: "Write at least 3 letters" })
+    .max(20, { message: "Big text write at max 20 letters" }),
+  email: z.email("Write correct mail").refine((value) => {
+    const lower = value.toLowerCase();
+    return lower.endsWith("@gmail.com") || lower.endsWith("@yahoo.com");
+  }, "Email must end on @gmail.com or @yahoo.com"),
+  subject: z
+    .string()
+    .trim()
+    .min(3, { message: "Write at least 3 letters" })
+    .max(20, { message: "Big text write at max 20 letters" }),
+  message: z.string().trim().min(30, { message: "Write at least 30 letters" }),
+});
+
 const InputBox = () => {
   return (
     <section className="bg-white rounded-xl px-5 py-8">
