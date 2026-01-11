302d8c67ae583f3306a5f76cd5398d86e322d5d4
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-11 12:23:38 +0400

    so I added one more thing in email schema to make ssure it will be written in lower letters not upper since it is an email + design started to become better but it is not finished yet

--- src/components/contact/InputBox.tsx
+++ src/components/contact/InputBox.tsx
@@ -8,10 +8,15 @@ const getInTouchSchema = z.object({
     .trim()
     .min(3, { message: "Write at least 3 letters" })
     .max(20, { message: "Big text write at max 20 letters" }),
-  email: z.email("Write correct mail").refine((value) => {
-    const lower = value.toLowerCase();
-    return lower.endsWith("@gmail.com") || lower.endsWith("@yahoo.com");
-  }, "Email must end on @gmail.com or @yahoo.com"),
+  email: z
+    .email("Write correct mail")
+    .refine((value) => value.toLowerCase(), {
+      message: "Use only lowercase letters",
+    })
+    .refine(
+      (value) => value.endsWith("@gmail.com") || value.endsWith("@yahoo.com"),
+      { message: "Email must end on @gmail.com or @yahoo.com" }
+    ),
   subject: z
     .string()
     .trim()
@@ -32,7 +37,10 @@ const InputBox = () => {
 
   const onSubmit = (data: GetInTouchSchema) => {
     console.log(data);
+    reset();
   };
+  const inputClass =
+    "w-full py-3 border border-gray-300 mt-5 rounded-md px-5 outline-none bg-gray-100";
   return (
     <section className="bg-white rounded-xl px-5 py-8">
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -41,15 +49,35 @@ const InputBox = () => {
           <legend className="text-center font-bold text-xl">
             Contact information
           </legend>
-          <input type="text" {...register("name")} />
-          <input type="email" {...register("email")} />
+          <input
+            type="text"
+            {...register("name")}
+            className={inputClass}
+            placeholder="Your Name"
+          />
+          <input
+            type="email"
+            {...register("email")}
+            className={inputClass}
+            placeholder="your.email@example.com"
+          />
         </fieldset>
         <fieldset>
           <legend className="text-center font-bold text-xl">
             Message Details
           </legend>
-          <input type="text" {...register("subject")} />
-          <input type="text" {...register("message")} />
+          <input
+            type="text"
+            {...register("subject")}
+            className={inputClass}
+            placeholder=""
+          />
+          <input
+            type="text"
+            {...register("message")}
+            className={inputClass}
+            placeholder=""
+          />
         </fieldset>
         <button type="submit">Send Message</button>
       </form>
