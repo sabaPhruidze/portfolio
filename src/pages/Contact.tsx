778a6f1c2d86d19b2761e527ce0edd8102b80cb2
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 11:05:49 +0400

    added now connect component which will consist moving on my linkedin and github page and than iframe of location

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,14 @@
 import ContactDetails from "../components/contact/ContactDetails";
 import GetInTouch from "../components/contact/GetInTouch";
 import InputBox from "../components/contact/InputBox";
-
+import Connect from "../components/contact/Connect";
 const Contact = () => {
   return (
     <div className="px-5 pt-10 bg-gray-100 h-400">
       <GetInTouch />
       <InputBox />
       <ContactDetails />
+      <Connect />
     </div>
   );
 };
