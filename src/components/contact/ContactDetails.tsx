bed5a6a949014b7522b6a17c8f0b6eef6ae5636f
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 11:03:13 +0400

    I remembered that email and address for SEO must be inside address so I added it

--- src/components/contact/ContactDetails.tsx
+++ src/components/contact/ContactDetails.tsx
@@ -5,24 +5,40 @@ const ContactDetails = () => {
   return (
     <section className="bg-white rounded-xl px-5 py-8 mt-10">
       <h2 className="font-bold text-xl">Contact Details</h2>
-      <div className="flex flex-row items-center gap-3 mt-5">
-        <div className="w-10 h-10 bg-blue-100 flex justify-center items-center rounded-full">
-          <img src={email} alt="email icon" className="w-6 h-6" />
+      <address className="not-italic">
+        <div className="flex flex-row items-center gap-3 mt-5">
+          <div className="w-10 h-10 bg-blue-100 flex justify-center items-center rounded-full">
+            <img src={email} alt="email icon" className="w-6 h-6" />
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-gray-500">Email</h3>
+            <a
+              className="font-bold hover:underline"
+              href="mailto:sabaphruidze01@gmail.com"
+              aria-label="Send me the email by clicking here"
+            >
+              sabaphruidze01@gmail.com
+            </a>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <h3 className="text-gray-500">Email</h3>
-          <p className="font-bold">sabaphruidze01@gmail.com</p>
+        <div className="flex flex-row items-center gap-3 mt-5">
+          <div className="w-10 h-10 bg-blue-100 flex justify-center items-center rounded-full">
+            <img src={pin} alt="email icon" className="w-6 h-6" />
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-gray-500">Location</h3>
+            <a
+              className="font-bold hover:underline"
+              href="https://www.google.com/maps/place/%E1%83%97%E1%83%91%E1%83%98%E1%83%9A%E1%83%98%E1%83%A1%E1%83%98/@41.6387676,44.7176433,9.5z/data=!4m6!3m5!1s0x40440cd7e64f626b:0x61d084ede2576ea3!8m2!3d41.6938026!4d44.8015168!16zL20vMGJtNGo?entry=ttu&g_ep=EgoyMDI2MDEwNy4wIKXMDSoASAFQAw%3D%3D"
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="You can open the location in google maps"
+            >
+              Georgia, Tbilisi
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row items-center gap-3 mt-5">
-        <div className="w-10 h-10 bg-blue-100 flex justify-center items-center rounded-full">
-          <img src={pin} alt="email icon" className="w-6 h-6" />
-        </div>
-        <div className="flex flex-col">
-          <h3 className="text-gray-500">Email</h3>
-          <p className="font-bold">sabaphruidze01@gmail.com</p>
-        </div>
-      </div>
+      </address>
     </section>
   );
 };
