0ab6c2ee0474a60910d96775dc415fbfac001809
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 11:36:08 +0400

    done iframe is also added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,13 +2,15 @@ import ContactDetails from "../components/contact/ContactDetails";
 import GetInTouch from "../components/contact/GetInTouch";
 import InputBox from "../components/contact/InputBox";
 import Connect from "../components/contact/Connect";
+import Location from "../components/contact/Location";
 const Contact = () => {
   return (
-    <div className="px-5 pt-10 bg-gray-100 h-400">
+    <div className="px-5 pt-10 bg-gray-100 h-500">
       <GetInTouch />
       <InputBox />
       <ContactDetails />
       <Connect />
+      <Location />
     </div>
   );
 };
